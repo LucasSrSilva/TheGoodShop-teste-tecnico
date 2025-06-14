183918a79a434dd30773113fc4be199dfed3c3f1
Lucas Souza Ribeiro da Silva <lucasrsdeven@gmail.com>
2025-06-14 16:17:38 -0300

    feat: better cart layout

--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -55,31 +55,39 @@ const ProductCard: React.FC<ProductCardProps> = ({
                     <Toast message={toast.message} type={toast.type} />
                 </div>
             )}
+
+
             <img
                 src={image}
                 alt={name}
                 className="w-full h-44 object-cover rounded-md"
             />
+
             <h2 className="text-xl mt-3 mb-1 font-semibold">{name}</h2>
+
             <div className="font-medium text-lg mb-2">
                 R$ {price.toFixed(2)}
             </div>
-            <div className="text-sm text-gray-600 mb-2">
-                category: {category}
-            </div>
-            <div className="text-sm text-yellow-500 mb-2">
-                Avaliação: {rating} ★
-            </div>
-            <div>
-                {tags.map(tag => (
-                    <span
-                        key={tag}
-                        className="inline-block bg-gray-100 text-gray-600 rounded-full px-3 py-1 text-xs mr-2 mb-1"
-                    >
-                        #{tag}
-                    </span>
-                ))}
-            </div>
+            {!isCart && (
+                <>
+                    <div className="text-sm text-gray-600 mb-2">
+                        category: {category}
+                    </div>
+                    <div className="text-sm text-yellow-500 mb-2">
+                        Avaliação: {rating} ★
+                    </div>
+                    <div>
+                        {tags.map(tag => (
+                            <span
+                                key={tag}
+                                className="inline-block bg-gray-100 text-gray-600 rounded-full px-3 py-1 text-xs mr-2 mb-1"
+                            >
+                                #{tag}
+                            </span>
+                        ))}
+                    </div>
+                </>
+            )}
             {isCart ? (
                 <button
                     onClick={handleRemoveFromCart}
